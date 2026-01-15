68640c4aab016f53ca1ac3a657f3154b9c17dbf0
mohamed <mojahdontheway@gmail.com>
2026-01-15 12:10:24 +0100

    add connection the database

---
+++ src/database.php
@@ -0,0 +1,26 @@
+<?php
+
+class Database
+{
+    private static $instance = null;
+
+    public static function getConnection()
+    {
+        if (self::$instance === null) {
+            $config = require __DIR__ . '/../config/database.php';
+
+            self::$instance = new PDO(
+                "mysql:host={$config['host']};dbname={$config['dbname']};charset=utf8",
+                $config['user'],
+                $config['password'],
+                [
+                    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+                ]
+            );
+        }
+
+        return self::$instance;
+    }
+}
+
+echo "ok";
